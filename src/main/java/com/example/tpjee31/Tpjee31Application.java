31b175f95f271662cf706c07e0af5840928057cf
ayoub <ayoubnassiri0@gmail.com>
2024-03-24 15:56:02 +0000

    second commit

--- src/main/java/com/example/tpjee31/Tpjee31Application.java
+++ src/main/java/com/example/tpjee31/Tpjee31Application.java
@@ -1,13 +1,32 @@
 package com.example.tpjee31;
 
+import com.example.tpjee31.dao.entities.Professor;
+import com.example.tpjee31.dao.repositories.CourseRepository;
+import com.example.tpjee31.dao.repositories.ProfessorRepository;
+import com.example.tpjee31.dao.repositories.SessionRepository;
+import com.example.tpjee31.dao.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tpjee31Application {
+public class Tpjee31Application implements CommandLineRunner {
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private ProfessorRepository professorRepository;
+    @Autowired
+    private SessionRepository sessionRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Tpjee31Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
